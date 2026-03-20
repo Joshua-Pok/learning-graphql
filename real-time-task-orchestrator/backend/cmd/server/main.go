405e044a69dfa88e7abd9e0d265aa3bc13ed6206
Joshua-Pok <official.joshuapok@gmail.com>
2026-03-20 14:26:32 +0800

    working server

--- real-time-task-orchestrator/backend/cmd/server/main.go
+++ real-time-task-orchestrator/backend/cmd/server/main.go
@@ -7,28 +7,23 @@ import (
 	"github.com/99designs/gqlgen/graphql/handler"
 	"github.com/99designs/gqlgen/graphql/playground"
 	"github.com/Joshua-Pok/task-orchestrator/internal/gql"
+	"github.com/Joshua-Pok/task-orchestrator/internal/gql/generated"
 	"github.com/Joshua-Pok/task-orchestrator/internal/task"
 )
 
 func main() {
-
 	store := &task.Store{}
-
 	resolver := &gql.Resolver{
 		Store: store,
 	}
 
-	//graphql endpoint
 	srv := handler.NewDefaultServer(
-
-		gql.NewExecutableSchema(gql.Config{Resolvers: resolver}),
+		generated.NewExecutableSchema(generated.Config{Resolvers: resolver}),
 	)
 
-	http.Handle("/query", srv) // graphql endpoint
-
-	http.Handle("/", playground.Handler("Graphql playground", "/query"))
+	http.Handle("/query", srv)
+	http.Handle("/", playground.Handler("GraphQL Playground", "/query"))
 
 	log.Println("Server running at http://localhost:8080/")
 	log.Fatal(http.ListenAndServe(":8080", nil))
-
 }
--- real-time-task-orchestrator/backend/internal/gql/resolver.go
+++ real-time-task-orchestrator/backend/internal/gql/resolver.go
@@ -1,7 +1,32 @@
 package gql
 
-import "github.com/Joshua-Pok/task-orchestrator/internal/task"
+import (
+	"github.com/Joshua-Pok/task-orchestrator/internal/task"
+)
 
 type Resolver struct {
 	Store *task.Store
 }
+
+// type queryResolver struct{ *Resolver }
+//
+// func (r *Resolver) Query() generated.QueryResolver {
+// 	return &queryResolver{r}
+// }
+//
+// func (r *queryResolver) Tasks(ctx context.Context) ([]*generated.Task, error) {
+// 	tasks := r.Store.List()
+//
+// 	var result []*generated.Task
+//
+// 	for _, t := range tasks {
+// 		result = append(result, &generated.Task{
+// 			ID:     t.ID.String(),
+// 			Title:  t.Title,
+// 			Status: t.Status,
+// 		})
+// 	}
+//
+// 	return result, nil
+//
+// }
